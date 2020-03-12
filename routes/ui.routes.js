e793c9ef77a54121af6ad80710195bc51358cddf
Rumsan Associates <depenti.karki@rumsan.com>
2020-03-12 12:59:29 +0545

    adding team page

--- routes/ui.routes.js
+++ routes/ui.routes.js
@@ -2,7 +2,12 @@ const router = require("express").Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Homepage - RumsanTech" });
+});
+
+/* GET team page */
+router.get("/team", function(req, res, next) {
+  res.render("team", { title: "Our Team - RumsanTech" });
 });
 
 module.exports = router;

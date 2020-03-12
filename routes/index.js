e0a524ec00b690e1bad85a3e7664c39fc7697f30
Rumsan Associates <depenti.karki@rumsan.com>
2020-03-12 11:03:15 +0545

    changing routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const router = require("express").Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const uiRouter = require("./ui.routes");
+const apiRouter = require("./api.routes");
+
+router.use("/", uiRouter);
+router.use("/api/v1", apiRouter);
 
 module.exports = router;
